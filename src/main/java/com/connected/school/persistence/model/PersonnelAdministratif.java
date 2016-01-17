294bf4d66b31863c4e2d2cc60ab4fae01a56f4f6
mohsine <mohsinebenallal@gmail.com>
2016-01-17 23:10:37 +0000

    third commit

--- src/main/java/com/connected/school/persistence/model/PersonnelAdministratif.java
+++ src/main/java/com/connected/school/persistence/model/PersonnelAdministratif.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+//c'est un changement
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
